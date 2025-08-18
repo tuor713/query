1a2c10585a0320acb16ab1d66394b6198c236d13
tuor713 <vmahrwald@gmail.com>
2025-08-17 21:45:11 -0400

    chore: Package upgrades

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,16 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
   plugins: [sveltekit(), nodePolyfills()],
+  esbuild: {
+    target: "es2022",
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "es2022",
+    },
+  },
   build: {
+    target: "es2022",
     commonjsOptions: {
       include: [/node_modules/],
       transformMixedEsModules: true,
