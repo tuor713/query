f8ffe635bcda73033388e9e7dffb28d7fc0b337b
tuor713 <vmahrwald@gmail.com>
2026-01-17 22:38:51 -0500

    chore: Bump version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,7 +29,7 @@
     // required patch in wvlet package to resolve package.json issue
     import { WvletCompiler } from "@wvlet/wvlet";
 
-    const VERSION = "0.0.16";
+    const VERSION = "0.0.17";
 
     let backendUrl = window.location.origin;
     if (import.meta.env.DEV) {
