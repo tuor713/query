8a54236edaca150b75f66eb7ca4038e28d14be0a
tuor713 <vmahrwald@gmail.com>
2026-05-10 21:02:11 -0400

    chore: Bump version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -46,7 +46,7 @@
     console.log("Initializing SaneQL");
     saneql.default(SANEQL_WASM);
 
-    const VERSION = "0.0.27";
+    const VERSION = "0.0.28";
 
     let backendUrl = window.location.origin;
     if (import.meta.env.DEV) {
